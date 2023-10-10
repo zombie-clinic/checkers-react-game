815a5429e42e9af6652a7e65a92d1aead7047579
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2023-10-11 02:28:57 +0300

    fetched data object keys access fail

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,16 +1,13 @@
 // import { useState } from 'react'
 import css from'./App.module.css'
 import CheckersBoard from '../components/CheckersBoard/CheckersBoard'
-// import EightByEightTable from '../components/EightByEightTable/EightByEightTable'
 
 function App() {
-
 
   return (
     <>
       <div className = {css.appContainer}>
         <CheckersBoard/>
-        {/* <EightByEightTable/> */}
       </div>
     </>)
 }
---
+++ src/api/CheckersApi.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+// const API_KEY = 'PUT IT HERE IF NEEDED';
+const BASE_URL = 'http://localhost:8080/move/';
+
+export const CheckersApi = async (gameId = '7304942c-1bfd-4c23-8c83-c9902a866807') => {
+  const url = `${BASE_URL}${gameId}`;
+  const { data } = await axios.get(url);
+  return data;
+};
