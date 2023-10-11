a9066f08b765cd7eadb1c332a77cd4e3a12e2e24
VladislavVolyanskiy <vladislavvolyanskiy@gmail.com>
2023-10-12 00:14:15 +0300

    fix CheckersBoard component, apiData removed

--- src/components/CheckersBoard/CheckersBoard.jsx
+++ src/components/CheckersBoard/CheckersBoard.jsx
@@ -10,25 +10,21 @@ import checkerData from '../../data/data.json';
 const CheckerBoard = () => {
 const [black, setBlack] = useState(checkerData.black);
 const [white, setWhite] = useState(checkerData.white);
-  // const [black, setBlack] = useState(null);
-  // const [white, setWhite] = useState(null);
-
-  const [apiData, setApiData] = useState(null);
 
   useEffect(() => {
     async function fetchData() {
       try {
         const data = await CheckersApi();
-        setApiData(data); // Update state with API data
-          // setBlack(apiData.state.black)
-          // setWhite(data.state.white)
+          setBlack(data.state.black)
+          setWhite(data.state.white)
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     }
-
+    
     fetchData();
   }, []); // Empty dependency array to run this effect only once when the component mounts
+
 
   const getCellColor = (row, col) => {
     if ((row + col) % 2 === 0) {
@@ -37,6 +33,7 @@ const [white, setWhite] = useState(checkerData.white);
       return 'black';
     }
   };
+
 
   const renderTable = () => {
     let blackCellCounter = 0;
@@ -67,28 +64,12 @@ const [white, setWhite] = useState(checkerData.white);
     ));
   };
 
-  console.log('this is apiData value');
-  console.log(apiData);
-
-  // const {gameId, state} = apiData;
-  // console.log(gameId);
-  // console.log(Object.keys(apiData)); //error
-  // console.log(JSON.parse(JSON.stringify(apiData)));
-  // console.log(black);
-  // console.log(white);
 
   return (
     <div>
     <table className={styles.checkerboard}>
       <tbody>{renderTable()}</tbody>
     </table>
-    {/* Render API data, if available */}
-    {apiData && (
-        <div>
-          <h2>API Data State(simlpy taken from fetch data)</h2>
-          {<pre>{apiData.state}</pre>}
-        </div>
-      )}
       <div>
       <h2>Checkers Data</h2>
       <pre>{JSON.stringify(checkerData)}</pre>
