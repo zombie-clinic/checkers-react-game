a46d74dd3f724ffe65bef1cd9e9e44a1db5689b8
Yevgen <yevgen.gryaznov@gmail.com>
2025-04-21 20:57:04 +0200

    Implement import board state feature (#25)

--- src/components/StartPage/StartPage.jsx
+++ src/components/StartPage/StartPage.jsx
@@ -1,27 +1,43 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom'; // импортируем для навигации
-import { startNewLobby, joinGame } from '../../api/CheckersApi';
+import { useNavigate } from 'react-router-dom';
+import { startNewLobby, joinGame, startImportedGame } from '../../api/CheckersApi';
 import styles from './StartPage.module.css';
 
 function StartPage() {
   const [gameId, setGameId] = useState(null);
   const [inputGameId, setInputGameId] = useState('');
-  // const [gameStarted, setGameStarted] = useState(false);
-  // const [side, setSide] = useState('');
-  // const [playerId, setPlayerId] = useState();
-  const navigate = useNavigate(); // Используем навигацию
+  const [importGameState, setImportGameState] = useState('');
+  const navigate = useNavigate();
 
   const handleStartGame = async () => {
     try {
       const newGameData = await startNewLobby(1, 'LIGHT');
       if (newGameData?.gameId) {
         setGameId(newGameData.gameId);
-        // setSide('LIGHT');
-        // setPlayerId(1);
-        // setGameStarted(true);
 
+        const startingState = newGameData.startingState || '';
         navigate(`/game/${newGameData.gameId}`, {
-          state: { side: 'LIGHT', playerId: 1 },
+          state: { side: 'LIGHT', playerId: 1, startingState },
+        });
+      }
+    } catch (error) {
+      console.error('Error starting a new game:', error);
+    }
+  };
+
+  const handleImportGame = async () => {
+    try {
+      const state = JSON.parse('{"dark":[1,2],"light":[21,17]}');
+      // const state = JSON.parse(importGameState);
+      const newGameData = await startImportedGame(1, 'LIGHT', state);
+      if (newGameData?.gameId) {
+        setGameId(newGameData.gameId);
+
+        // todo what would be the default starting state (instead of empty string)
+        // see also the same method in handleStartGame
+        const startingState = newGameData.startingState || '';
+        navigate(`/game/${newGameData.gameId}`, {
+          state: { side: 'LIGHT', playerId: 1, startingState },
         });
       }
     } catch (error) {
@@ -31,18 +47,21 @@ function StartPage() {
 
   const handleJoinGame = async () => {
     try {
-      const storedGameId = inputGameId || gameId;
+      const storedGameId = inputGameId || gameId;      
+      
       if (!storedGameId) {
         alert('You must enter a game ID!');
         return;
       }
       setGameId(storedGameId);
-      // setSide('DARK');
-      // setPlayerId(2);
-      // setGameStarted(true);
 
+
+      const joinGameData = await joinGame(storedGameId, 2);
+
+      const startingState = joinGameData.startingState || '';
+      console.log(`Joining game ${storedGameId} with starting state ${startingState}`);
       navigate(`/game/${storedGameId}`, {
-        state: { side: 'DARK', playerId: 2 },
+        state: { side: 'DARK', playerId: 2, startingState },
       });
     } catch (error) {
       console.error('Error joining game:', error);
@@ -62,6 +81,15 @@ function StartPage() {
             onChange={e => setInputGameId(e.target.value)}
           />
           <button onClick={handleJoinGame}>Join Game</button>
+        </div>
+        <div className={styles.joinGameContainer}>
+          <input
+            type="text"
+            placeholder="Enter game state"
+            value={importGameState}
+            onChange={e => setImportGameState(e.target.value)}
+          />
+          <button onClick={handleImportGame}>Import Game</button>
         </div>
       </div>
     </>
